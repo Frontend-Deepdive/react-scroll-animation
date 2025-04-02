c142df402a3f7ff2819ed14b21208702f15ee31e
abyss-s <lh3729@naver.com>
2025-04-02 13:35:12 +0900

    feat: framer-motion을 이용한 스크롤 애니메이션 구현

--- base-react/src/components/Box/Box.tsx
+++ base-react/src/components/Box/Box.tsx
@@ -1,9 +1,20 @@
 import './Box.css';
+import { motion } from 'framer-motion';
+
 const Box = () => {
   return (
-    <div>
-      <img src='https://picsum.photos/300' alt='random' />
-    </div>
+    <motion.div
+      initial={{ opacity: 0, y: 100 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: false }}
+      transition={{
+        ease: 'easeInOut',
+        duration: 1.2,
+        y: { duration: 1 },
+      }}
+    >
+      <img src='https://picsum.photos/400' alt='random' width='400px' height='auto' />
+    </motion.div>
   );
 };
 
