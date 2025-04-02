55fa885612c5c1c46cca24a61aefd8e066f51bea
silverain <ekdldkaa@naver.com>
2025-04-02 15:31:45 +0900

    fix : pass scaleY directly

--- base-react/src/components/ProgressBar.tsx
+++ base-react/src/components/ProgressBar.tsx
@@ -1,28 +1,22 @@
-import { useScroll, useTransform } from "motion/react";
-import { useEffect, useState } from "react";
+import { motion, useScroll } from "motion/react";
 import "./style.css";
 
 function ProgressBar() {
   const { scrollYProgress } = useScroll();
-
-  const [scrollY, setScrollY] = useState(0);
-
-  const status = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
-
-  useEffect(() => {
-    const unsubscribe = scrollYProgress.on("change", (latest) => {
-      setScrollY(latest);
-    });
-
-    return () => unsubscribe();
-  }, [scrollYProgress]);
-
   return (
-    <div className="fix">
-      <div
-        style={{ height: "1rem", width: status.get(), backgroundColor: "red" }}
-      ></div>
-      {status.get()}
+    <div>
+      <motion.div
+        style={{
+          height: "1rem",
+          scaleX: scrollYProgress,
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          originX: 0,
+          backgroundColor: "#ff0088",
+        }}
+      />
     </div>
   );
 }
