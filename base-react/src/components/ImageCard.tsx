2f6d886324cb01bed7097edd89140e874366ab99
neungdong <iyeseul877@gmail.com>
2025-04-03 18:44:47 +0900

    feat : useTransform으로 특정 구간에서 애니메이션 동작 구현

--- base-react/src/components/ImageCard.tsx
+++ base-react/src/components/ImageCard.tsx
@@ -1,4 +1,5 @@
-import { forwardRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef, forwardRef } from "react";
 
 interface ImageCardProps {
   imageUrl: string;
@@ -6,23 +7,26 @@ interface ImageCardProps {
 }
 
 const ImageCard = forwardRef<HTMLImageElement, ImageCardProps>(({ imageUrl, author }, ref) => {
+  const sectionRef = useRef<HTMLElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start start", "end start"],
+  });
+
+  const opacity = useTransform(scrollYProgress, [0.2, 0.5], [1, 0.5]);
+  const translateY = useTransform(scrollYProgress, [0.2, 0.5], [0, 180]);
+
   return (
-    <div style={cardWrapper}>
+    <motion.section ref={sectionRef} style={{ ...cardWrapper, opacity, translateY }}>
       <img src={imageUrl} alt={author} ref={ref} style={thumbnailImage} />
       <div style={cardContent}>
         <p>{author}</p>
       </div>
-    </div>
+    </motion.section>
   );
 });
 
 export default ImageCard;
-
-export const cardGridWrapper = {
-  display: "grid",
-  gap: "20px",
-  padding: "20px",
-};
 
 const cardWrapper = {
   width: "400px",
