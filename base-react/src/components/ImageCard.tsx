7fc0c08fc0dc0e0412452c5ed784001abcdf1911
neungdong <iyeseul877@gmail.com>
2025-04-03 16:21:17 +0900

    feat : ImageCard 컴포넌트 구현

--- base-react/src/components/ImageCard.tsx
+++ base-react/src/components/ImageCard.tsx
@@ -1,29 +1,30 @@
-import { forwardRef } from "react";
-
 interface ImageCardProps {
   imageUrl: string;
   author: string;
 }
 
-// eslint-disable-next-line no-undef
-const ImageCard = forwardRef<HTMLImageElement, ImageCardProps>(({ imageUrl, author }, ref) => {
+const ImageCard = ({ imageUrl, author }: ImageCardProps) => {
   return (
     <div style={cardWrapper}>
-      <img src={imageUrl} alt={author} ref={ref} style={thumbnailImage} />
+      <img src={imageUrl} alt={author} style={thumbnailImage} />
       <div style={cardContent}>
         <p>{author}</p>
       </div>
     </div>
   );
-});
+};
 
-ImageCard.displayName = "ImageCard";
 export default ImageCard;
 
+export const cardGridWrapper = {
+  display: "grid",
+  gap: "20px",
+  padding: "20px",
+};
+
 const cardWrapper = {
-  width: "100%",
-  maxWidth: "500px",
-  marginBottom: "20px",
+  width: "400px",
+  margin: "200px auto",
   backgroundColor: "white",
   borderRadius: "10px",
   overflow: "hidden",
@@ -33,7 +34,7 @@ const cardWrapper = {
 const thumbnailImage = {
   width: "100%",
   height: "50%",
-  objectFit: "cover" as const,
+  objectFit: "contain" as const,
 };
 
 const cardContent = {
